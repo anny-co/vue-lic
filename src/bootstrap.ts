2a5771f50cfcc96aed9f3a83f18b242361e79ae2
Alexander Bartolomey <occloxium@gmail.com>
2021-10-01 12:37:17 +0200

    feat: implement bootstrap function
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ src/bootstrap.ts
@@ -0,0 +1,37 @@
+import { getOptions } from "./options";
+import registerGlobals from "./register-globals";
+import { load } from "./utils";
+
+export default async (): Promise<void> => {
+  const {
+    onReady,
+    onError,
+    globalObjectName,
+    globalDataStore,
+    config,
+    customResourceURL,
+    deferScriptLoad,
+    disableScriptLoad,
+  } = getOptions();
+
+  registerGlobals();
+
+  if (disableScriptLoad) {
+    return;
+  }
+
+  try {
+    // data list for pids
+    window[globalDataStore] = window[globalDataStore] || [];
+    window[globalDataStore].push(config.id);
+
+    await load(globalObjectName, customResourceURL, deferScriptLoad, true);
+    if (onReady) {
+      onReady(window[globalObjectName]);
+    }
+  } catch (err) {
+    if (onError) {
+      onError(err as Error);
+    }
+  }
+};
