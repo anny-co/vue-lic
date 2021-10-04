0f4412ebac492aa4a114f85aa50a07de9830d8a3
Alexander Bartolomey <occloxium@gmail.com>
2021-10-04 15:25:05 +0200

    chore!: export only esm
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- bili.config.ts
+++ bili.config.ts
@@ -1,22 +1,15 @@
 import { Config } from "bili";
 import { name } from "./package.json";
 
-const isDev = process.env.NODE_ENV === "development";
-
 const config: Config = {
   input: "./src/index.ts",
   externals: ["vue"],
   plugins: {
-    typescript2: {
-      // Override the config in `tsconfig.json`
-      tsconfigOverride: {
-        include: ["src"],
-      },
-    },
+    typescript2: {},
   },
   output: {
     dir: "./dist/",
-    format: isDev ? ["esm"] : ["esm", "cjs", "umd", "umd-min"],
+    format: ["esm"],
     moduleName: name,
   },
 };
